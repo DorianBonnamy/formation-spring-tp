3b234e2094f9fdd32714e178fb0ef06620e64023
Dorian <dorian.bonnamy@gmail.com>
2019-03-08 10:26:29 +0100

    TP1 finished ObjectFactory created This factory permit to create new SiteService or new CaptorService

--- src/main/java/com/training/springcore/BigCorpApplication.java
+++ src/main/java/com/training/springcore/BigCorpApplication.java
@@ -1,5 +1,8 @@
 package com.training.springcore;
 
+import com.training.springcore.bigcorp.ObjectFactory;
+import com.training.springcore.service.SiteService;
+
 public class BigCorpApplication {
 
     public static void main (String[] args){
@@ -9,5 +12,9 @@ public class BigCorpApplication {
 
     public void run(){
         System.out.println("Application startup");
+
+        ObjectFactory objectFactory = new ObjectFactory();
+        SiteService siteService = objectFactory.createSiteService();
+        System.out.println(siteService.findById("siteA"));
     }
 }
